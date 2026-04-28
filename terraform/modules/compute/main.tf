0b4ef586247dc463ee1caa317dae3e75c50548aa
jamaloflagos <toyinjamal@gmail.com>
2026-04-28 17:16:24 +0100

    fix: ecs task frequent downtime

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -253,7 +253,7 @@ resource "aws_ecs_service" "main" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.main.arn
   launch_type     = "FARGATE"
-  desired_count   = 1
+  desired_count   = 2
 
   network_configuration {
     subnets          = var.subnet_ids
@@ -268,7 +268,7 @@ resource "aws_ecs_service" "main" {
   }
 
   health_check_grace_period_seconds  = 60
-  deployment_minimum_healthy_percent = 50
+  deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
   enable_execute_command             = true
 
